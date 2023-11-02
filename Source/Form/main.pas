a21a42084a24e432edff802d2ccf0f94cbf6676d
Adem KOCAMAZ <kocamazadem@gmail.com>
2023-11-02 20:15:57 +0300

    İlk Yükleme

---
+++ Source/Form/main.pas
@@ -0,0 +1,26 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TForm_Main = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  Form_Main: TForm_Main;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
